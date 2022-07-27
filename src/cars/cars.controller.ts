8ff7f13e807d63488abae8a8fdd0688fafbeef08
Christian <chrmesina@icloud.com>
2022-07-27 14:25:50 -0400

    First Commit

---
+++ src/cars/cars.controller.ts
@@ -0,0 +1,43 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseUUIDPipe,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { CarsService } from './cars.service';
+
+@Controller('cars')
+export class CarsController {
+  constructor(private readonly carsService: CarsService) {}
+
+  @Get()
+  getAllCars() {
+    return this.carsService.findAll();
+  }
+
+  @Get(':id')
+  getCarById(@Param('id', ParseUUIDPipe) id: string) {
+    return this.carsService.findOneById(id);
+  }
+
+  @Post()
+  createCar(@Body() body: any) {
+    return body;
+  }
+  @Patch(':id')
+  updateCar(@Param('id') id: string, @Body() body: any) {
+    return body;
+  }
+
+  @Delete(':id')
+  deleteCar(@Param('id') id: string) {
+    return {
+      method: 'delete',
+      id,
+    };
+  }
+}
